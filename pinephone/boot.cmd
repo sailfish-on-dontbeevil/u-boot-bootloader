e0cdee88ea4961865f59a1dc68055806ce572445
Dylan Van Assche <dylan.van.assche@protonmail.com>
2020-01-12 10:13:21 +0100

    [u-boot] Updated boot scripts for plymouth-lite

--- pinephone/boot.cmd
+++ pinephone/boot.cmd
@@ -1,6 +1,6 @@
 echo Setting bootargs
-setenv bootargs console=tty0 console=ttyS0,115200 root=/dev/mmcblk0p2 no_console_suspend rootwait 
-earlycon=uart,mmio32,0x01c28000 panic=10 consoleblank=0 loglevel=1
+setenv bootargs console=tty0 console=ttyS0,115200 root=/dev/mmcblk0p2 no_console_suspend rootwait quiet
+earlycon=uart,mmio32,0x01c28000 panic=10 consoleblank=0 loglevel=0
 printenv
 echo Loading DTB
 load mmc 0:1 ${fdt_addr_r} /sun50i-a64-pinephone.dtb
