fe5b71e19873dd2761155370483446a4cb542780
Dylan Van Assche <dylan.van.assche@protonmail.com>
2020-03-20 20:09:04 +0100

    Fix eMMC booting

--- pinephone/boot.cmd
+++ pinephone/boot.cmd
@@ -1,11 +1,24 @@
+gpio set 114
+
+if test ${mmc_bootdev} -eq 0 ; then
+	echo "Booting from SD";
+	setenv bootdev 0;
+else
+	echo "Booting from eMMC";
+	setenv bootdev 2;
+fi;
+
 echo Setting bootargs
-setenv bootargs console=tty0 console=ttyS0,115200 root=/dev/mmcblk0p2 no_console_suspend rootwait quiet
+setenv bootargs console=tty0 console=ttyS0,115200 root=/dev/mmcblk${bootdev}p2 no_console_suspend rootwait quiet
 earlycon=uart,mmio32,0x01c28000 panic=10 consoleblank=0 loglevel=0
 printenv
 echo Loading DTB
 load mmc 0:1 ${fdt_addr_r} /sun50i-a64-pinephone.dtb
 echo Loading kernel Image
 load mmc 0:1 ${kernel_addr_r} /Image
+gpio set 115
+
 echo Booting kernel NOW
+gpio set 116
 booti ${kernel_addr_r} - ${fdt_addr_r}
 
