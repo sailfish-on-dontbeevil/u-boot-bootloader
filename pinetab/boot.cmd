5c1444acb207ac9f6fd590f42353f66561b9c7b2
Adam Pigg <adam@piggz.co.uk>
2020-12-31 14:56:51 +0000

    Update boot.cmd

--- pinetab/boot.cmd
+++ pinetab/boot.cmd
@@ -1,5 +1,8 @@
 echo Setting bootargs
-setenv bootargs console=tty0 console=ttyS0,115200 root=/dev/mmcblk${devnum}p2 no_console_suspend rootwait fbcon=rotate:1 earlycon=uart,mmio32,0x01c28000 panic=10 consoleblank=0
+setenv rootfs_part 2
+part uuid ${devtype} ${devnum}:${rootfs_part} uuid
+setenv bootargs console=tty0 console=ttyS0,115200 root=PARTUUID=${uuid} no_console_suspend rootwait fbcon=rotate:1 earlycon=uart,mmio32,0x01c28000 panic=10 consoleblank=0
+echo Environment:
 printenv
 echo Loading DTB
 load ${devtype} ${devnum}:${distro_bootpart} ${fdt_addr_r} /sun50i-a64-pinetab.dtb
