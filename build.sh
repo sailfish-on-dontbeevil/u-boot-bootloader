87e9c7d2fb5f9a79fbde5b3071a478c1ecdd4821
Adam Pigg <adam@piggz.co.uk>
2022-01-30 20:37:52 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@
 echo "Building ARM Trusted Firmware (ATF)..."
 cd arm-trusted-firmware
 make PLAT=sun50i_a64 bl31
+make PLAT=rk3399
 export BL31=../arm-trusted-firmware/build/sun50i_a64/release/bl31.bin
 
 echo "Building U-boot for A64 with ATF"
 cd ../u-boot
 make clean
 make pinephone_defconfig
+make pinephone-pro-rk3399_defconfig
 make
