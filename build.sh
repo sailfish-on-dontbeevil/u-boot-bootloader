ce821c92d9a10eb866f65f8d6c43227928bf9cb8
Adam Pigg <adam@piggz.co.uk>
2022-01-30 21:36:47 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 echo "Building ARM Trusted Firmware (ATF)..."
 cd arm-trusted-firmware
 make PLAT=sun50i_a64 bl31
-make PLAT=rk3399
+M0_CROSS_COMPILE="arm-linux-gnu-" make PLAT=rk3399
 export BL31=../arm-trusted-firmware/build/sun50i_a64/release/bl31.bin
 
 echo "Building U-boot for A64 with ATF"
