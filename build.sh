78cc52c21ed0218b47d349cb1137efb6cb9d89fe
Adam Pigg <adam@piggz.co.uk>
2022-01-31 19:53:08 +0000

    Add bl31 for ppp

--- build.sh
+++ build.sh
@@ -5,11 +5,12 @@ echo "Building ARM Trusted Firmware (ATF)..."
 cd arm-trusted-firmware
 make PLAT=sun50i_a64 bl31
 make PLAT=rk3399
-export BL31=../arm-trusted-firmware/build/sun50i_a64/release/bl31.bin
 
 echo "Building U-boot for A64 with ATF"
 cd ../u-boot
 make clean
+export BL31=../arm-trusted-firmware/build/sun50i_a64/release/bl31.bin
 make pinephone_defconfig
+export BL31=../arm-trusted-firmware/build/rk3399/release/bl31/bl31.elf
 make pinephone-pro-rk3399_defconfig
 make
